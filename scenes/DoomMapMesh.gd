82f3d8362de3c819a30341756bab8aa6722dc16e
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2026-01-22 16:16:58 -0500

    add prog

--- scenes/DoomMapMesh.gd
+++ scenes/DoomMapMesh.gd
@@ -6,83 +6,83 @@ func _ready():
 	pass
 
 
-func load_map(map: RawDoomMap) -> void:
-	var surface_array = []
-	surface_array.resize(Mesh.ARRAY_MAX)
-
-	# PackedVector**Arrays for mesh construction.
-	var verts = PackedVector3Array()
-	var uvs = PackedVector2Array()
-	var normals = PackedVector3Array()
-	var indices = PackedInt32Array()
-	
-	var sectorsPolygons: Array[Array] = []
-	var totalFloorVertexSize: int = 0
-	
-	var vertexPackage: Dictionary = get_map_sector_vertices(map)
-	sectorsPolygons = vertexPackage.sectorsPolygons
-	totalFloorVertexSize = vertexPackage.totalFloorVertexSize
-	
-	
-	for i in range(2):
-		for sectorInd: int in sectorsPolygons.size():
-			for polygonInd: int in sectorsPolygons[sectorInd][0].size():
-				for pointInd: int in sectorsPolygons[sectorInd][0][polygonInd].size():
-					var baseVert: Vector3 = sectorsPolygons[sectorInd][i][polygonInd][pointInd]
-					verts.append(baseVert/100.0)
-					normals.append(Vector3.UP*(i*-2+1))
-					if i == (1):
-						pass
-					uvs.append(Vector2(i, i))
-					#print(verts.size())
-					#print(normals.size())
-					
-	
-	var vertCount: int = 0
-	for sectorInd: int in sectorsPolygons.size():
-		for polygonInd: int in sectorsPolygons[sectorInd][0].size():
-			var polygon3D: PackedVector3Array = sectorsPolygons[sectorInd][0][polygonInd]
-			var polygon2D: PackedVector2Array = PackedVector2Array()
-			for point: Vector3 in polygon3D:
-				polygon2D.append(Vector2(point.x, point.z))
-			var triInds: PackedInt32Array = Geometry2D.triangulate_polygon(polygon2D)
-			#print(triInds)
-			#print(totalFloorVertexSize)
-			var ceilTriInds: Array = []
-			for triInd: int in triInds.size():
-				ceilTriInds.append(triInds[triInd]+vertCount)
-			
-			var vecCeilTriInds: Array = ceilTriInds.map(func(i): return verts[i])
-			print(ceilTriInds)
-			print(vecCeilTriInds)
-			
-			triInds.reverse()
-			var floorTriInds: Array = []
-			for triInd: int in triInds.size():
-				floorTriInds.append(triInds[triInd]+vertCount+totalFloorVertexSize)
-			
-			var vecFloorTriInds: Array = floorTriInds.map(func(i): return verts[i])
-			print(floorTriInds)
-			print(vecFloorTriInds)
-			
-			indices.append_array(ceilTriInds)
-			indices.append_array(floorTriInds)
-			vertCount+= polygon3D.size()
-			print()
-
-	# Assign arrays to surface array.
-	surface_array[Mesh.ARRAY_VERTEX] = verts
-	#surface_array[Mesh.ARRAY_TEX_UV] = uvs
-	surface_array[Mesh.ARRAY_NORMAL] = normals
-	surface_array[Mesh.ARRAY_INDEX] = indices
-	
-	print(totalFloorVertexSize)
-	print(verts.size())
-	print(uvs.size())
-	print(normals.size())
-	print(indices.size())
-	
-	# Create mesh surface from mesh array.
-	# No blendshapes, lods, or compression used.
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
-	ResourceSaver.save(mesh, "res://map.tres", ResourceSaver.FLAG_COMPRESS)
+#func load_map(map: RawDoomMap) -> void:
+	#var surface_array = []
+	#surface_array.resize(Mesh.ARRAY_MAX)
+#
+	## PackedVector**Arrays for mesh construction.
+	#var verts = PackedVector3Array()
+	#var uvs = PackedVector2Array()
+	#var normals = PackedVector3Array()
+	#var indices = PackedInt32Array()
+	#
+	#var sectorsPolygons: Array[Array] = []
+	#var totalFloorVertexSize: int = 0
+	#
+	#var vertexPackage: Dictionary = get_map_sector_vertices(map)
+	#sectorsPolygons = vertexPackage.sectorsPolygons
+	#totalFloorVertexSize = vertexPackage.totalFloorVertexSize
+	#
+	#
+	#for i in range(2):
+		#for sectorInd: int in sectorsPolygons.size():
+			#for polygonInd: int in sectorsPolygons[sectorInd][0].size():
+				#for pointInd: int in sectorsPolygons[sectorInd][0][polygonInd].size():
+					#var baseVert: Vector3 = sectorsPolygons[sectorInd][i][polygonInd][pointInd]
+					#verts.append(baseVert/100.0)
+					#normals.append(Vector3.UP*(i*-2+1))
+					#if i == (1):
+						#pass
+					#uvs.append(Vector2(i, i))
+					##print(verts.size())
+					##print(normals.size())
+					#
+	#
+	#var vertCount: int = 0
+	#for sectorInd: int in sectorsPolygons.size():
+		#for polygonInd: int in sectorsPolygons[sectorInd][0].size():
+			#var polygon3D: PackedVector3Array = sectorsPolygons[sectorInd][0][polygonInd]
+			#var polygon2D: PackedVector2Array = PackedVector2Array()
+			#for point: Vector3 in polygon3D:
+				#polygon2D.append(Vector2(point.x, point.z))
+			#var triInds: PackedInt32Array = Geometry2D.triangulate_polygon(polygon2D)
+			##print(triInds)
+			##print(totalFloorVertexSize)
+			#var ceilTriInds: Array = []
+			#for triInd: int in triInds.size():
+				#ceilTriInds.append(triInds[triInd]+vertCount)
+			#
+			#var vecCeilTriInds: Array = ceilTriInds.map(func(i): return verts[i])
+			#print(ceilTriInds)
+			#print(vecCeilTriInds)
+			#
+			#triInds.reverse()
+			#var floorTriInds: Array = []
+			#for triInd: int in triInds.size():
+				#floorTriInds.append(triInds[triInd]+vertCount+totalFloorVertexSize)
+			#
+			#var vecFloorTriInds: Array = floorTriInds.map(func(i): return verts[i])
+			#print(floorTriInds)
+			#print(vecFloorTriInds)
+			#
+			#indices.append_array(ceilTriInds)
+			#indices.append_array(floorTriInds)
+			#vertCount+= polygon3D.size()
+			#print()
+#
+	## Assign arrays to surface array.
+	#surface_array[Mesh.ARRAY_VERTEX] = verts
+	##surface_array[Mesh.ARRAY_TEX_UV] = uvs
+	#surface_array[Mesh.ARRAY_NORMAL] = normals
+	#surface_array[Mesh.ARRAY_INDEX] = indices
+	#
+	#print(totalFloorVertexSize)
+	#print(verts.size())
+	#print(uvs.size())
+	#print(normals.size())
+	#print(indices.size())
+	#
+	## Create mesh surface from mesh array.
+	## No blendshapes, lods, or compression used.
+	#mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
+	#ResourceSaver.save(mesh, "res://map.tres", ResourceSaver.FLAG_COMPRESS)
--- scenes/doom_sector_geometry.gd
+++ scenes/doom_sector_geometry.gd
@@ -28,8 +28,31 @@ func init_sector(rawSector: RawDoomMap.DoomSector, sectorId: int, sectorGeo: Arr
 	lightLevel = rawSector.lightLevel
 	build_sector_meshes(sectorGeo)
 
+func add_polygon_to_ceil(tridPoly: PackedVector3Array) -> void:
+	for polyPoint: Vector3 in tridPoly:
+		polyPoint.y = ceilHeight
+		ceilMesh.mesh.surface_set_normal(Vector3.DOWN)
+		ceilMesh.mesh.surface_set_uv(Vector2.ZERO)
+		ceilMesh.mesh.surface_add_vertex(polyPoint)
+
+func add_polygon_to_floor(tridPoly: PackedVector3Array) -> void:
+	for polyPoint: Vector3 in tridPoly:
+		polyPoint.y = floorHeight
+		floorMesh.mesh.surface_set_normal(Vector3.DOWN)
+		floorMesh.mesh.surface_set_uv(Vector2.ZERO)
+		floorMesh.mesh.surface_add_vertex(polyPoint)
+
 func build_sector_meshes(sectorGeo: Array[PackedVector2Array]) -> void:
-	pass
+	ceilMesh.mesh.clear_surfaces()
+	floorMesh.mesh.clear_surfaces()
+	ceilMesh.mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLES)
+	for polygonPoints: PackedVector2Array in sectorGeo:
+		var triangulatedPoly: Array[Vector2] = Array(Geometry2D.triangulate_polygon(polygonPoints)).map(func(i): return polygonPoints[i])
+		var heightedPoly: PackedVector3Array = PackedVector3Array(triangulatedPoly.map(func(v): return Vector3(v.x, 0, v.y)))
+		add_polygon_to_ceil(heightedPoly)
+		add_polygon_to_floor(heightedPoly)
+	ceilMesh.mesh.surface_end()
+	floorMesh.mesh.surface_end()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
